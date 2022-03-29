6d44005756d0427e48a88a7e2b51c8333fa95f19
puffcooks <mlopez0104788@gmail.com>
2022-03-29 19:43:44 -0400

    finalize mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,3 +94,23 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+}
